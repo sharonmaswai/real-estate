fbb559e6de093d437c417e9f9950a5f7e483f454
sharonmaswai <chepmaswai@gmail.com>
2019-03-07 10:43:57 +0300

    add javascript code

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+function search() {
+  var name=document.getElementById('id').value;
+  var licenseNumber= document. getElementById('id').value;
+
+  var stoneNames=["Gerald-Fero","Steve-Kari","Mandi-Sato"]
+  for(var index=0; index < stoneNames.length; index+= 1) {
+    if(name===stoneNumbers[index])
+    {
+      Document.getElementById("").innerHTML="An agent of Stone Properties."
+    } esle {
+      Document.getElementById('id').innerHTML="Not an Agent of Stone Properties."
+
+    }
+  }
+
+
+}
